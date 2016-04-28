261efaabc57e3adced7ca7c64b52887e415590f1
Rick Jaffe <rjaffe@berkeley.edu>
2016-04-28 15:36:54 -0700

    NOJIRA: FY2015-16_Q3 changes

---
+++ RDM_metrics_genericized.py
@@ -0,0 +1,68 @@
+import csv, sys
+from collections import Counter
+import config_v2
+
+# View now produces Date/Time End column, so the row no.'s have shifted
+
+# Column headers (row to be removed):
+#    [0] Title ("Question asked")
+#    [1] Date/Time (Start)
+#    [2] Date/Time (End)
+#    [3] Is this question answered?
+#    [4] Referral In
+#    [5] Referral Out
+#    [6] Department
+#    [7] Organizational partners
+#    [8] Patron status
+
+
+filename = sys.argv[1]
+report_period = sys.argv[2]
+myrows = []
+
+# Read data into a list of lists, clean as required
+with open(filename) as csvfile:
+    for row in csv.reader(csvfile, delimiter=","):
+        # Remove rows in which "Is this question answered?" is blank -- i.e., 'FYI only' engagements
+        if not row[3]: continue
+
+        # Referrals in, Referrals out, plus controlled-vocabulary \
+        # Department, Organizational partner and Patron status "columns":
+        # replace empty values with appropriate label
+        for n, label in zip(config_v2.dictable_cols, config_v2.labels):
+            if row[n] == '':
+                row[n] = label
+            # make every cell into a list (some cells have comma separated values)
+            row[n] = row[n].split(', ')
+        # Replace individual names with the corresponding org name in Referral In and Referral Out fields
+        # (positional elements [3] and [4])
+        for n in [4, 5]:
+            ref_x = row[n]
+            row[n] = []  # Empty cell to ready it for being re-filled
+            for term in ref_x:
+                for key in config_v2.refs.keys():
+                    if term in config_v2.refs[key][1]:
+                        term = config_v2.refs[key][0]
+                        row[n].append(term)
+        myrows.append(row)
+    # Remove header row -- it throws off counts.
+    myrows = myrows[1:]
+
+# ***** COUNT THE DATA *****
+# Each row (list) except first (header) row represents a consulting engagement
+print('\nIn %s, RDM Consulting provided %d consultations.' % (report_period, (len(myrows) - 1)))
+
+# Count how many engagements are resolved successfully
+yesrows = [r for r in myrows if r[3] == 'Yes, we have an answer!']
+print('We reached a successful resolution in %d of those engagements.' % len(yesrows))
+
+# Gather and count (subtotal) the values for referrals in, referrals out, departments,
+# organizational partners and patron status (only those five "columns" for now).
+for i, n in enumerate(config_v2.dictable_cols):
+    counter = Counter()
+    for row in myrows:
+        for z in row[n]:
+            counter[z] += 1
+    print('\n' + config_v2.headings[i] + ':')
+    for (k, v) in counter.most_common():
+        print(k + ' (' + str(v) + ')')
