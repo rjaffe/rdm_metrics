e196b420b4738dc4e8a8be24625e78bfb1d83d36
Rick Jaffe <rjaffe@berkeley.edu>
2016-08-22 17:41:46 -0700

    Add list elements (names) to dictionaries ... to account for FY2015-16 Q4 cases

--- config_v2.py
+++ config_v2.py
@@ -5,18 +5,21 @@ dictable_cols = [4, 5, 6, 7, 8, 9]
 # let the data do the heavy lifting...everything is in this dict!
 refs = {
     'lib': ['The Library', ['Harrison Dekker', 'Jamie Wittenberg', 'Susan Edwards', 'Steve Mendoza', 'Steven Mendoza',
-                            'Margaret Phillips', 'data-consult list (Library)', 'Brian Quigley']],
-    'css': ['Campus Shared Services - IT', ['Brett Larsen', 'Daniel Bass']],
+                            'Margaret Phillips', 'data-consult list (Library)', 'Brian Quigley', 'Library',
+                            'Data Storage/Sharing and the Social Sciences Working Group', 'Erik Mitchell', 'Susan Powell']],
+    'css': ['Campus Shared Services - IT', ['Brett Larsen', 'Daniel Bass', 'Johnathon Kogelman', 'CSS-IT']],
     'dlab': ['D-Lab', ['D-Lab Consulting List', 'D-Lab', 'Jon Stiles', 'Zawadi Rucks Ahidiana']],
     'scf': ['Statistical Computing Facility', ['Chris Paciorek', 'Ryan Lovett']],
     'brc': ['Berkeley Research Computing',
-            ['Patrick Schmitz', 'Aron Roberts', 'Aaron Culich', 'Jason Christopher', 'Kelly Rowland',
+            ['Patrick Schmitz', 'Aron Roberts', 'Aaron Culich', 'Jason Christopher', 'Kelly Rowland', 'Gary Jung',
              'BRC Cloud Consulting']],
     'dh': ['Digital Humanities @ Berkeley', ['Quinn Dombrowski', 'Camille Villa', 'Digital Humanities']],
     'rdm': ['RDM Consulting', ['researchdata@berkeley.edu', 'Rick Jaffe', 'Chris Hoffman']],
     'cdl': ['California Digital Library', ['Joan Starr', 'Stephanie Simms']],
-    'ist': ['Information Services & Technology - API', ['Jennifer Bellenger', 'Jon Broshious', 'Ian Crew', 'Jon Hays']],
-    'micronet': ['Micronet', ['micronet', 'Micronet', 'micronet list', 'Micronet list']]
+    'ist': ['Information Services & Technology - API', ['Jennifer Bellenger', 'Jon Broshious', 'Ian Crew', 'Jon Hays', 'bConnected']],
+    'micronet': ['Micronet', ['micronet', 'Micronet', 'micronet list', 'Micronet list']],
+    'iao': ['Industry Alliances Office', ['Nicole Hensley', 'Nicole Hensley (IAO/IPIRA)']],
+    'ssw': ['School of Social Welfare', ['David Fullmer']]
 }
 
 labels = ['', 'None', 'Unknown department', 'Consultation(s) without a partner', 'Unknown', '']
