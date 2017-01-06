0b8f8ff965ce2d01234e49b95d15c918607dec68
Rick Jaffe <rjaffe@berkeley.edu>
2017-01-05 16:26:49 -0800

    Update refs dictionary contents, added BIDS to refs

--- config_v2.py
+++ config_v2.py
@@ -6,20 +6,26 @@ dictable_cols = [4, 5, 6, 7, 8, 9, 10]
 refs = {
     'lib': ['The Library', ['Harrison Dekker', 'Jamie Wittenberg', 'Susan Edwards', 'Steve Mendoza', 'Steven Mendoza',
                             'Margaret Phillips', 'data-consult list (Library)', 'Brian Quigley', 'Library',
-                            'Data Storage/Sharing and the Social Sciences Working Group', 'Erik Mitchell', 'Susan Powell']],
-    'css': ['Campus Shared Services - IT', ['Brett Larsen', 'Daniel Bass', 'Johnathon Kogelman', 'CSS-IT']],
-    'dlab': ['D-Lab', ['D-Lab Consulting List', 'D-Lab', 'Jon Stiles', 'Zawadi Rucks Ahidiana']],
+                            'Data Storage/Sharing and the Social Sciences Working Group', 'Erik Mitchell',
+                            'Susan Powell', 'Anna Sackmann']],
+    'css': ['Campus Shared Services - IT', ['Brett Larsen', 'Daniel Bass', 'Johnathon Kogelman', 'Johnathon Kogelman (CSS-IT)',
+                                            'CSS-IT']],
+    'dlab': ['D-Lab', ['D-Lab Consulting List', 'D-Lab', 'Jon Stiles', 'Zawadi Rucks Ahidiana',
+                       'Rick Jaffe (via D-Lab Consulting web page)']],
     'scf': ['Statistical Computing Facility', ['Chris Paciorek', 'Ryan Lovett']],
     'brc': ['Berkeley Research Computing',
             ['Patrick Schmitz', 'Aron Roberts', 'Aaron Culich', 'Jason Christopher', 'Kelly Rowland', 'Gary Jung',
-             'BRC Cloud Consulting']],
-    'dh': ['Digital Humanities @ Berkeley', ['Quinn Dombrowski', 'Camille Villa', 'Digital Humanities']],
-    'rdm': ['RDM Consulting', ['researchdata@berkeley.edu', 'Rick Jaffe', 'Chris Hoffman']],
+             'BRC Cloud Consulting', 'Jason Huff (Computational Genomics Resource Lab)', 'Berkeley Research Computing - Cloud']],
+    'dh': ['Digital Humanities @ Berkeley', ['Quinn Dombrowski', 'Camille Villa', 'Digital Humanities', 'Claudia Natalia Von Vacano']],
+    'rdm': ['RDM Consulting', ['researchdata@berkeley.edu', 'Rick Jaffe', 'Chris Hoffman', 'John B Lowe',
+                               'BRC Survey 2016 (Response to follow-up from Jamie)']],
     'cdl': ['California Digital Library', ['Joan Starr', 'Stephanie Simms']],
-    'ist': ['Information Services & Technology - API', ['Jennifer Bellenger', 'Jon Broshious', 'Ian Crew', 'Jon Hays', 'bConnected']],
+    'ist': ['Information Services & Technology - API', ['Jennifer Bellenger', 'Jon Broshious', 'Ian Crew', 'Jon Hays',
+                                                        'bConnected', 'Michael Leefers']],
     'micronet': ['Micronet', ['micronet', 'Micronet', 'micronet list', 'Micronet list']],
     'iao': ['Industry Alliances Office', ['Nicole Hensley', 'Nicole Hensley (IAO/IPIRA)']],
-    'ssw': ['School of Social Welfare', ['David Fullmer']]
+    'ssw': ['School of Social Welfare', ['David Fullmer']],
+    'bids': ['Berkeley Institute for Data Science', ['BIDS']]
 }
 
 labels = ['', 'None', 'Unknown department', 'Unknown division', 'Consultation(s) without a partner', 'Unknown', '']
