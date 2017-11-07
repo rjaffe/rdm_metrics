bbe2a26bdef4ecd1e6c8ffe5eeff379207cd52a9
Rick Jaffe <rjaffe@berkeley.edu>
2017-11-06 16:54:27 -0800

    More updates and minor fixes

---
+++ Metrics_args/RDM_metrics_args-ALL-through-FY2018-Q1.py
@@ -0,0 +1,6 @@
+# These are the arguments provided to the script
+filepath = '/Users/rjaffe/Documents/RDM/RDM_Metrics/MetricsData/RITConsultingEngagementLog_Google-downloads/'
+filename = 'Research-IT-Consulting-Engagements-Log_20171019_1544PDT.csv'
+report_period_descriptor = 'the period from January 1, 2015 through September 30, 2017'
+report_start = '2015-01-01'
+report_end = '2017-09-30'
