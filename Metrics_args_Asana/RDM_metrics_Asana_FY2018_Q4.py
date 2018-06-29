60a2af772d7e3ebb40aeaeaf3e7490b2bec4590e
Rick Jaffe <rjaffe@berkeley.edu>
2018-06-28 23:58:49 -0700

    commit renamed file

---
+++ Metrics_args_Asana/RDM_metrics_Asana_FY2018_Q4.py
@@ -0,0 +1,8 @@
+# These are the arguments provided to the script
+# Replace filepath with path on your machine
+
+filepath = '/Users/rjaffe/Documents/RDM/RDM_Metrics/MetricsData/data-from-asana/June2018-Metrics/'
+filename = 'Consultations_20180626-1706PT.csv'
+report_period_descriptor = 'the period from April 1, 2018 through June 30, 2018'
+report_start = '2018-04-01'
+report_end = '2018-06-30'
